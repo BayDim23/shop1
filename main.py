3a46be2530c45d538d7f1fe399e21d698c87865a
BayDim23 <168500336+BayDim23@users.noreply.github.com>
2024-06-21 06:10:44 +0300

    add new code

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-class Store:
+class Store(object):
     def __init__(self, name,address):
         self.name = name
         self.address = address
         self.items = {}
     def add_item(self, item_name, price):
         self.items[item_name] = price
+        print(f"товар {item_name} добавлен в магазин {self.name}")
+
+
     def remove_item(self, item_name):
         if item_name in self.items:
             del self.items[item_name]
-        else:
-            print("Нет продуктов с таким названием")
+            print(f"товар {item_name} удален из {self.name}")
 
     def get_price(self, item_name):
         return self.items.get(item_name)
@@ -17,26 +19,31 @@ class Store:
     def update_price(self, item_name, new_price):
         if item_name in self.items:
             self.items[item_name] = new_price
+            print(f"цена на товар {item_name} обновлена в магазине {self.name}")
         else:
-            print("Нет продуктов с таким названием")
+            print(f"товар {item_name} не найден в магазине {self.name}")
 
-    def info(self):
-        print(f"Название магазина: {self.name}")
-        print(f"Адрес магазина: {self.address}")
-        print(f"Список продуктов:")
-        for item, price in self.items.items():
-            print(f"{item}: {price}")
 
-# Создаем объекты класса Store
 store1 = Store("Магазин Фруктов", "ул. Первомайская, 10")
-store1.add_item("яблоки", 0.5)
-store1.add_item("бананы", 0.75)
-
 store2 = Store("Овощной Рай", "пр. Ленина, 25")
-store2.add_item("помидоры", 1.0)
-store2.add_item("огурцы", 0.8)
-
 store3 = Store("Домашний Магазин", "ул. Садовая, 5")
-store3.add_item("молоко", 1.2)
-store3.add_item("хлеб", 0.6)
+
+# Создаем объекты класса Stor
+
+store1.add_item("яблоки", 60)
+store1.add_item("бананы", 80)
+
+
+store2.add_item("помидоры", 58)
+store2.add_item("огурцы", 67)
+
+
+store3.add_item("молоко", 49)
+store3.add_item("хлеб", 54)
+
+store1.remove_item("яблоки")
+
+print(store1.get_price("бананы"))
+store1.update_price("бананы", 70)
+
 
